01c3db9ea121969006ac8acf77cf08e55f35f0c1
Leticia Attoh <leticiaattoh1994@gmail.com>
2024-10-21 09:38:44 +0000

    committin branch2

--- src/services/product.js
+++ src/services/product.js
@@ -4,5 +4,10 @@ export const apiGetProducts = async () => apiClient.get("/products");
 
 export const apiGetSingleProduct = async (slug) => apiClient.get(`/products/${slug}`);
 
-export const apiAddProduct = async (payload) => apiClient.get("/products", payload);
+export const apiAddProduct = async (payload) => apiClient.post("/products", payload);
 
+export const apiEditProduct = async (payload) => {
+    return apiClient.post("dashboard/edit_ad",payload)
+}
+
+// delete using id format
