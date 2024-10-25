bea24dddfc44a9aaeee4d2012515b91824dd176c
JoelCann <joelcann12@gmail.com>
2024-10-25 12:57:20 +0000

    Fixed netlify issue

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import MyAds from "./pages/dashboard/MyAds.jsx";
 import AllAds from "./pages/landingPage/AllAds.jsx";
 import DashboardLayout2 from "./layouts/DashboardLayout2.jsx";
 import MyAds2 from "./pages/dashboard/MyAds2.jsx";
+import PostAdvert from "./components/postAdvert.jsx";
 
 function App() {
   const router = createBrowserRouter([
